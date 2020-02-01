fe92676e9d9d6b7b175655231e3234c8adcc573e
Swastik Panda <swastikpanda32@gmail.com>
2020-02-01 22:16:11 +0530

    Added state and updated setState to the component

--- src/index.js
+++ src/index.js
@@ -16,12 +16,19 @@ import ReactDOM from "react-dom";
 // CLASS COMPONENT
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
     );
-    return <div>Lattitude: </div>;
+  }
+  render() {
+    return <div>Lattitude:{this.state.lat}</div>;
   }
 }
 
